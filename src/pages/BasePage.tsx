d7b342f3b204a174bda95e9ecfbcec73b136e8db
wwontk <wwontk@gmail.com>
2023-12-18 16:55:40 +0900

    composition 및 extraction

--- src/pages/BasePage.tsx
+++ src/pages/BasePage.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
 import Clock from './2-4/Clock';
+import Composition from './2-5/Composition';
+import Extraction from './2-5/Extraction';
 
 function BasePage() {
-  return (<><div>Hello React</div><Clock /></>);
+  const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'http://placekitten.com/g/64/64'
+    }
+  };
+  return (
+    <>
+      <div>Hello React</div>
+      {/* <Clock /> */}
+      <Composition name="sara" />
+      <Extraction avatarUrl={comment.author.avatarUrl} name={comment.author.name} text={comment.text} date={comment.date} />
+    </>
+  );
 }
 
 export default BasePage;
