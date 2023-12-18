2bbab2f32669ffac532ae332a282014dc786166c
wwontk <wwontk@gmail.com>
2023-12-18 15:56:06 +0900

    first commit

---
+++ src/BasePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BasePage() {
+  return (
+    <div>
+      Hello React
+    </div>
+  );
+}
+
+export default BasePage;
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BasePage from './BasePage';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <BasePage />
+  </React.StrictMode>
+);
