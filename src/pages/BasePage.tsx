68d786fdaecc59cf66db88bc1ae87c5b9b986d25
wwontk <wwontk@gmail.com>
2023-12-18 23:36:11 +0900

    list map 활용

--- src/pages/BasePage.tsx
+++ src/pages/BasePage.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 
-import Clock from './2-4/Clock';
-import Composition from './2-5/Composition';
-import Extraction from './2-5/Extraction';
-import ClassClock from './2-6/ClassClock';
-import Event from './2-7/Event';
-import Greeting from './2-8/Greeting';
+// import Clock from './2-4/Clock';
+// import Composition from './2-5/Composition';
+// import Extraction from './2-5/Extraction';
+// import ClassClock from './2-6/ClassClock';
+// import Event from './2-7/Event';
+// import Greeting from './2-8/Greeting';
+import List from './2-9/List';
+
 
 function BasePage() {
-  const comment = {
-    date: new Date(),
-    text: 'I hope you enjoy learning React!',
-    author: {
-      name: 'Hello Kitty',
-      avatarUrl: 'http://placekitten.com/g/64/64'
-    }
-  };
+  // const comment = {
+  //   date: new Date(),
+  //   text: 'I hope you enjoy learning React!',
+  //   author: {
+  //     name: 'Hello Kitty',
+  //     avatarUrl: 'http://placekitten.com/g/64/64'
+  //   }
+  // };
   return (
     <>
       <div>Hello React</div>
-      <Greeting />
+      <List />
     </> 
   );
 }
