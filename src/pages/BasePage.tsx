6fba1f1e814e234d4300aef12809d67856394430
wwontk <wwontk@gmail.com>
2023-12-18 16:17:37 +0900

    clock 렌더링

--- src/pages/BasePage.tsx
+++ src/pages/BasePage.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
+import Clock from './2-4/Clock';
+
 function BasePage() {
-  return (
-    <div>
-      Hello React
-    </div>
-  );
+  return (<><div>Hello React</div><Clock /></>);
 }
 
 export default BasePage;
